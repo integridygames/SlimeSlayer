ab6d54ceb41e2b6adfe1517cf7412b764e5fd38a
MrSoTniK <sotsotdv2019@gmail.com>
2022-07-28 03:26:12 +0300

    Создание отсутствующих инсталлеров

---
+++ Assets/Scripts/Game/Installers/FactoriesInstaller.cs
@@ -0,0 +1,15 @@
+using Game.Gameplay.Factories;
+using Game.Gameplay.Models;
+using Zenject;
+
+namespace Game.Installers 
+{
+    public class FactoriesInstaller : MonoInstaller
+    {
+        public override void InstallBindings()
+        {
+            Container.Bind<LevelFactory>().AsSingle();
+            Container.Bind<ApplicationData>().FromFactory<ApplicationDataFactory>().AsSingle();
+        }
+    }
+}
