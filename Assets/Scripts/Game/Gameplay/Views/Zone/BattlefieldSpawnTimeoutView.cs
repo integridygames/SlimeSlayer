3a0b376b03676d8f3f42cf674da42cd35dd7b889
dmayorov <dmayorov@gear-games.com>
2022-11-25 13:29:00 +0300

    EnemySpawn 4

---
+++ Assets/Scripts/Game/Gameplay/Views/Zone/BattlefieldSpawnTimeoutView.cs
@@ -0,0 +1,15 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace Game.Gameplay.Views.Zone
+{
+    public class BattlefieldSpawnTimeoutView : MonoBehaviour
+    {
+        [SerializeField] private TMP_Text _timeout;
+
+        public float CurrentTimeout
+        {
+            set => _timeout.text = value.ToString("n2");
+        }
+    }
+}
--- Assets/Scripts/Game/Gameplay/Views/Zone/BattlefieldZoneView.cs
+++ Assets/Scripts/Game/Gameplay/Views/Zone/BattlefieldZoneView.cs
@@ -6,14 +6,18 @@ namespace Game.Gameplay.Views.Zone
     public class BattlefieldZoneView : ZoneView
     {
         [SerializeField] private BattlefieldSpawnSettingsRecord[] _battlefieldSpawnSettings;
-        [SerializeField] private Bounds _bounds;
 
+        [SerializeField] private Bounds _bounds;
         [SerializeField] private float _spawnTime;
+        [SerializeField] private BattlefieldSpawnTimeoutView _battlefieldSpawnTimeoutView;
 
         public IReadOnlyList<BattlefieldSpawnSettingsRecord> BattlefieldSpawnSettings =>
             _battlefieldSpawnSettings;
+
         public Bounds Bounds => _bounds;
         public float SpawnTime => _spawnTime;
+
+        public BattlefieldSpawnTimeoutView BattlefieldSpawnTimeoutView => _battlefieldSpawnTimeoutView;
 
         private void OnDrawGizmos()
         {
